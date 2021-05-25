a179838ff5157a8c18c9c179b53b7308e670168f
genkiasai <baseballboy1025@gmail.com>
2021-05-25 16:21:58 +0900

    backup

---
+++ memo/image/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>image</title>
+</head>
+<body>
+    <div class="nomal-image-area">
+        <!-- <img class="nomal-image" src="./image.jpg" alt=""> -->
+    </div>
+    <div class="fix-image-area">
+        <!-- <img class="fix-image" src="./image.jpg" alt=""> -->
+    </div>
+</body>
+</html>
